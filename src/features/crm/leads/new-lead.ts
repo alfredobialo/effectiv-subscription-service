e933d7fa32707e0ce77dd1d4a9c5bc2eaac13d26
Alfred Obialo <alfredcsdinc@gmail.com>
2025-07-03 12:34:35 +0100

    New Files

---
+++ src/features/crm/leads/new-lead.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'new-lead',
+  imports: [],
+  template: `
+    <p>
+      new-lead works!
+    </p>
+  `,
+  styles: ``
+})
+export class NewLead {
+
+}
---
+++ src/features/inventory/stock/view-stock-document.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'view-stock-document',
+  imports: [],
+  template: `
+    <p>
+      view-stock-document works!
+    </p>
+  `,
+  styles: ``
+})
+export class ViewStockDocument {
+
+}
